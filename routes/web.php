43ff523616c5734fda4e344d6bd936b6c19ed38c
IT-17005 <mnhruhan@gmail.com>
2020-10-08 10:37:01 +0600

    trial

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::post('/store','StudentsController@store')->name('store');
 Route::post('/delete/{id}','StudentsController@delete')->name('delete');
 Route::get('/edit/{id}','StudentsController@edit')->name('edit');
 Route::post('/update/{id}','StudentsController@update')->name('update');
+//Route::post('/update/{id}','StudentsController@update')->name('update');
+
 
